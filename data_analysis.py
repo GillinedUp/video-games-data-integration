146285a39aeef9ce94ef492f6a0ba124c9853a09
Yurii Vyzhha <vyzhha@student.agh.edu.pl>
2020-06-13 12:22:31 +0200

    added scenario 2

---
+++ data_analysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_list_of_genres(all_games_df):
+    return list(set(list(all_games_df['genres'].dropna())))
+
+def scenario_2(all_games_df):
+    non_game_genres = ['Audio Production', 'Photo Editing', 'Video Production', 'Web Publishing', 'Animation & Modeling', 'Game Development', 'Utilities', 'Design & Illustration']
+    
+    steam_reviews_df = all_games_df[['review_score', 'genres']].copy()
+    steam_reviews_df.replace(0, np.nan, inplace=True)
+    steam_reviews_df.dropna(inplace=True)
+    mean_steam_scores = steam_reviews_df.groupby(['genres']).mean().sort_values(by=['review_score']).round(decimals=2)
+    mean_steam_scores = mean_steam_scores.drop(non_game_genres)
+    mean_steam_scores.to_csv('mean_steam_scores.csv')
+
+    rawg_reviews = all_games_df[['rating', 'genres']].copy()
+    rawg_reviews.replace(0.0, np.nan, inplace=True)
+    rawg_reviews.dropna(inplace=True)
+    mean_rawg_scores = rawg_reviews.groupby(['genres']).mean().sort_values(by=['rating']).round(decimals=2)
+    mean_rawg_scores = mean_rawg_scores.drop(non_game_genres)
+    mean_rawg_scores.to_csv('mean_rawg_scores.csv')
+
+    metacritic_reviews = all_games_df[['metacritic', 'genres']].copy()
+    metacritic_reviews['metacritic'].replace(' ', np.nan, inplace=True)
+    metacritic_reviews.dropna(inplace=True)
+    metacritic_reviews.astype({'metacritic': np.int64}, inplace=True)
+    metacritic_reviews = metacritic_reviews.groupby(['genres']).mean().sort_values(by=['metacritic'])
+    metacritic_reviews = metacritic_reviews.drop(non_game_genres)
+    metacritic_reviews.to_csv('metacritic_reviews.csv')
+    
+    return mean_steam_scores, mean_rawg_scores, metacritic_reviews
+
