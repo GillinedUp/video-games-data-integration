27776ffe07e399043dfcbb9b31bf11060694049f
Yurii Vyzhha <vyzhha@student.agh.edu.pl>
2020-06-13 12:45:39 +0200

    added scenario 4

--- data_analysis.py
+++ data_analysis.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 
 def get_list_of_genres(all_games_df):
     return list(set(list(all_games_df['genres'].dropna())))
 
-def get_scenario_2_data(all_games_df):
+def get_mean_scores_per_genre(all_games_df):
     non_game_genres = ['Audio Production', 'Photo Editing', 'Video Production', 'Web Publishing', 'Animation & Modeling', 'Game Development', 'Utilities', 'Design & Illustration']
     
     steam_reviews_df = all_games_df[['review_score', 'genres']].copy()
@@ -30,4 +35,42 @@ def get_scenario_2_data(all_games_df):
     mean_metacritic_scores.to_csv('mean_metacritic_scores.csv')
     
     return mean_steam_scores, mean_rawg_scores, mean_metacritic_scores
+
+def merge_developer_and_publisher_data(all_games_df):
+    all_games_df['developer_steam'] = np.where(all_games_df['developer_steam'].isnull() 
+                                           & ~all_games_df['developer_rawg'].str.isspace(), 
+                                           all_games_df['developer_rawg'], 
+                                           all_games_df['developer_steam'])
+    all_games_df['publisher_steam'] = np.where(all_games_df['publisher_steam'].isnull() 
+                                           & ~all_games_df['publisher_rawg'].isnull(), 
+                                           all_games_df['publisher_rawg'], 
+                                           all_games_df['publisher_steam'])
+    return all_games_df
+
+def transform_data_for_regressor(all_games_df):
+    all_games_df = merge_developer_and_publisher_data(all_games_df)
+    games_non_zero_score = all_games_df[all_games_df['review_score'] != 0].copy()
+    data = games_non_zero_score[['developer_steam', 'publisher_steam', 'genres', 'released', 'tag1', 'tag2', 'tag3', 'tag4']].copy()
+    data_lowercase = data.apply(lambda x: x.astype(str).str.lower())
+    data_lowercase['released'] = data_lowercase['released'].apply(lambda x: str(x.year) if type(x) == pd._libs.tslibs.timestamps.Timestamp else '')
+    X_raw = data_lowercase.values
+    y = games_non_zero_score['review_score'].values
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc.fit(X_raw)
+    X = enc.transform(X_raw)
+    return X, y_test
+
+def split_data(X, y):
+    return train_test_split(X, y, test_size=0.2, random_state=42) 
+
+def train_svr(X_train, X_test, y_train, y_test)
+    regr = svm.SVR()
+    regr.fit(X_train, y_train)
+    y_pred = regr.predict(X_test)
+    return mean_absolute_error(y_test, y_pred)
+
+def get_simple_prediction_mean_absolute_error(y_test):
+    y_pred_mean = np.empty(y_test.shape)
+    y_pred_mean.fill(y_test.mean())
+    return mean_absolute_error(y_test, y_pred_mean)
 
